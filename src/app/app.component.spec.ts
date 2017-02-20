caa8a23b75b49e493ca14c1a5177abe77e616415
Steve Witman <stevewitman@gmail.com>
2017-02-20 14:08:27 -0700

    Add a couple tests on app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Todo } from './todo';
 import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
@@ -23,16 +24,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  // // it(`should have as title 'app works!'`, async(() => {
-  // //   const fixture = TestBed.createComponent(AppComponent);
-  // //   const app = fixture.debugElement.componentInstance;
-  // //   expect(app.title).toEqual('app works!');
-  // // }));
+  it(`should have a newTodo todo`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  }));
 
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  // }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
 });
